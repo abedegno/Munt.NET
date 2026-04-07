0961264399fc20454ac7f270e655f1dfe2b15dcb
Jon Williams <jon@jonwilliams.org.uk>
2026-04-08 00:35:51 +0100

    feat: add mt32emu P/Invoke declarations and return codes

---
+++ src/Mt32Emu.NET/Mt32EmuImports.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Mt32Emu.NET;
+
+internal static class Mt32EmuImports
+{
+    const string LibraryName = "libmt32emu";
+
+    [DllImport(LibraryName)]
+    public static extern IntPtr mt32emu_create_context(IntPtr reportHandler, IntPtr instanceData);
+
+    [DllImport(LibraryName)]
+    public static extern void mt32emu_free_context(IntPtr context);
+
+    [DllImport(LibraryName)]
+    public static extern Mt32EmuReturnCode mt32emu_add_rom_file(IntPtr context, string filename);
+
+    [DllImport(LibraryName)]
+    public static extern void mt32emu_set_stereo_output_samplerate(IntPtr context, double samplerate);
+
+    [DllImport(LibraryName)]
+    public static extern Mt32EmuReturnCode mt32emu_open_synth(IntPtr context);
+
+    [DllImport(LibraryName)]
+    public static extern void mt32emu_close_synth(IntPtr context);
+
+    [DllImport(LibraryName)]
+    public static extern Mt32EmuReturnCode mt32emu_play_msg(IntPtr context, uint msg);
+
+    [DllImport(LibraryName)]
+    public static extern unsafe Mt32EmuReturnCode mt32emu_play_sysex(IntPtr context, byte* sysex, uint len);
+
+    [DllImport(LibraryName)]
+    public static extern unsafe void mt32emu_render_bit16s(IntPtr context, short* stream, uint len);
+
+    [DllImport(LibraryName)]
+    public static extern int mt32emu_is_active(IntPtr context);
+}
---
+++ src/Mt32Emu.NET/Mt32EmuReturnCode.cs
@@ -0,0 +1,15 @@
+namespace Mt32Emu.NET;
+
+public enum Mt32EmuReturnCode
+{
+    OK = 0,
+    AddedControlRom = 1,
+    AddedPcmRom = 2,
+    AddedPartialControlRom = 3,
+    AddedPartialPcmRom = 4,
+    MissingRoms = -1,
+    FileNotFound = -2,
+    FileNotLoaded = -3,
+    NotOpened = -4,
+    QueueFull = -5,
+}
